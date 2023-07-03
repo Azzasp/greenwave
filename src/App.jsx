35e45328500d6e651668ba89aac233edba2d69ff
Azzasp <p.erickbatista@gmail.com>
2023-07-03 18:15:32 -0300

    Acompanhar button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import AcompanharButton from "./components/acompanharButton.jsx";
 
 function App() {
 
   return (
     <>
         <h1 className="text-red-600 text-9xl font-bold">Hello World</h1>
+        <AcompanharButton></AcompanharButton>
     </>
   )
 }
---
+++ src/components/acompanharButton.jsx
@@ -0,0 +1,15 @@
+
+function AcompanharButton(){
+    return(
+        <>
+            <button className="h-8 w-36 outline outline-2 outline-green-500 rounded
+                                font-semibold
+                                hover:bg-green-500 hover:text-white">
+                ACOMPANHAR
+            </button>
+        </>
+    )
+}
+
+
+export default AcompanharButton;
