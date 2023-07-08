ff39e7a7a9662547c8464005b37e6e82eaedd13b
Azzasp <p.erickbatista@gmail.com>
2023-07-08 16:49:41 -0300

    Refactoring Slideshow parallax

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,5 +1,43 @@
 import {useState} from "react";
 
+const ods15 = {
+    title: 'VIDA SOBRE A TERRA',
+    text: ' Juntos, podemos proteger a vida em nosso planeta.\n' +
+        'A ODS 15 busca preservar os ecossistemas terrestres, combater a\n' +
+        'desertificação e a degradação do solo, e promover a conservação da biodiversidade.\n' +
+        'É hora de agir!',
+    imageURL: [
+        './src/assets/ods_imgs/ods_15/sky.png',
+        './src/assets/ods_imgs/ods_15/lake.png',
+        './src/assets/ods_imgs/ods_15/terrain.png'
+    ]
+}
+
+const ods14 = {
+    title: 'VIDA DEBAIXO DA\n + AGUA',
+    text: 'Podemos proteger nossos oceanos e mares.\n' +
+        'A ODS 14 busca conservar e utilizar de forma sustentável\n' +
+        'os recursos marinhos, promovendo a vida na água.\n' +
+        'É hora de agir!',
+    imageURL: [
+        './src/assets/ods_imgs/ods_14/coral.png',
+        './src/assets/ods_imgs/ods_14/ocean.png',
+        './src/assets/ods_imgs/ods_14/fishes.png'
+    ]
+}
+
+const ods13 = {
+    title: 'COMBATE\n' + 'AS ALTERAÇÕES CLIMATICAS',
+    text: 'Juntos, podemos enfrentar o desafio climático.\n' +
+            'A ODS 13 busca combater as mudanças climáticas e seus impactos,\n' +
+            'promovendo medidas urgentes para preservar nosso planeta.\n' +
+            'É hora de agir!',
+    imageURL: [
+        './src/assets/ods_imgs/ods_13/ice.png',
+        './src/assets/ods_imgs/ods_13/mountain.png',
+        './src/assets/ods_imgs/ods_13/animals.png'
+    ]
+}
 
 export default function Slide(){
 
@@ -8,10 +46,8 @@ export default function Slide(){
 
     const [num, setNum] = useState(0);
 
-    const imageURL = [
-        './src/assets/ods13.png',
-        './src/assets/ods14.png',
-        './src/assets/ods15.png'
+    const imageList = [
+        ods13, ods14, ods15
     ]
 
     const iconURL = [
@@ -20,27 +56,30 @@ export default function Slide(){
     ]
 
     function upClick(){
-        if(num > imageURL.length -2) setNum(0);
+        if(num > imageList.length -2) setNum(0);
         else setNum(num + 1);
     }
 
     function downClick(){
-        if(num === 0) setNum(imageURL.length - 1);
+        if(num === 0) setNum(imageList.length - 1);
         else setNum(num - 1);
     }
 
 
     return(
         <>
-            <div
-                 className={`absolute w-full top-1/2 px-10
+            <div className={`absolute w-full top-1/2 px-10
                             flex justify-between justify-items-center`}>
                 <div onClick={downClick} style={{backgroundImage: `url(${iconURL.at(1)})`}}
                         className={`w-[25px] h-[25px] bg-cover bg-no-repeat bg-center left-0`}></div>
                 <div onClick={upClick} style={{backgroundImage: `url(${iconURL.at(0)})`}}
                         className={`w-[25px] h-[25px] bg-cover bg-no-repeat bg-center right-0`}></div>
             </div>
-            <div style={{backgroundImage: `url(${imageURL.at(num)})`}}
+            <div>
+                <h1><span>TERRA</span></h1>
+            </div>
+            <div style={{backgroundImage: `url(${imageList.at(num)
+                                                .imageURL.at(1)})`}}
                  className="w-screen h-screen bg-cover bg-no-repeat">
             </div>
 
